d4370e357c7656e73be296848204baf51ae567cf
down <twinkim11@naver.com>
2015-11-14 21:55:18 +0900

    adapter,facade

---
+++ smarthome/Light.java
@@ -0,0 +1,24 @@
+package com.j2.facade.smarthome;
+
+public class Light {
+String description; 
+
+public Light(String description) { 
+ this.description = description; 
+}
+
+public void on() { 
+ System.out.println(description + " on"); 
+} 
+public void off() {
+	System.out.println(description + " off"); 
+}
+
+public void dim(int level) { 
+	System.out.println(description + " dimming to " + level  + "%"); 
+} 
+public String toString() {
+	return description; 
+   }
+} 
+
